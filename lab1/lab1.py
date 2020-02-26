6b408750767be88b75e1bfe245795306d1f8fe80
Carlos Mata <carlosmatauniversidad@gmail.com>
2020-02-26 17:18:13 +0100

    Terminada la primera práctica de la asignatura

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -4,6 +4,7 @@ import sqlite3
 import re
 import urllib.request
 import os.path
+import dateutil.parser
 #from tkinter import *
 
 
@@ -34,12 +35,7 @@ def extraer_lista(file):
 
 def abrir_url(url, file):
     try:
-        if os.path.exists(file):
-            recarga = input("La página ya ha sido cargada. Desea recargarla (s/n)?")
-            if recarga == "s":
-                urllib.request.urlretrieve(url, file)
-        else:
-            urllib.request.urlretrieve(url, file)
+        urllib.request.urlretrieve(url, file)
         return file
     except:
         print ("Error al conectarse a la página")
@@ -68,6 +64,55 @@ def listarBD():
         cadenaListar += nombre + "\n" + link + "\n" + fecha + "\n\n"
     msg = messagebox.showinfo("Elementos BD", cadenaListar)
 
+def buscarMes():
+    ventanaBuscarMes = tk.Tk()
+    tk.Label(ventanaBuscarMes, text="Introduzca el mes (Xxx)").grid(row=0)
+    entradaMes = tk.Entry(ventanaBuscarMes)
+    entradaMes.grid(row=0, column=1)
+
+    def metodoDeBuscar(event):
+        mesABuscar = entradaMes.get()
+        conexionBuscarMes = sqlite3.connect('rssABC.db')
+        conexionBuscarMes.text_factory = str
+        sBuscar = "%" + mesABuscar + "%"
+        elementosBuscados = conexionBuscarMes.execute("SELECT ID, NOMBRE, LINK, FECHA FROM NOTICIA WHERE FECHA LIKE ?",(sBuscar,))
+        resultados = ""
+        for fila in elementosBuscados:
+            nombre = fila[1]
+            link = fila[2]
+            fecha = fila[3]
+            resultados += nombre + "\n" + link + "\n" + fecha + "\n\n"
+        conexionBuscarMes.close()
+        msg = messagebox.showinfo("Elementos BD por mes dado", resultados)
+
+    entradaMes.bind("<Return>", metodoDeBuscar)
+    ventanaBuscarMes.mainloop()
+
+def buscarDia():
+    ventanaBuscarDia = tk.Tk()
+    tk.Label(ventanaBuscarDia, text="Introduzca la fecha dd/mm/aaaa").grid(row=0)
+    entradaDia = tk.Entry(ventanaBuscarDia)
+    entradaDia.grid(row=0, column=1)
+
+    def metodoDeBuscar(event):
+        diaABuscar = entradaDia.get()
+        conexionBuscarDia = sqlite3.connect('rssABC.db')
+        conexionBuscarDia.text_factory = str
+        elementosBuscados = conexionBuscarDia.execute("SELECT ID, NOMBRE, LINK, FECHA FROM NOTICIA")
+        resultados = ""
+        for fila in elementosBuscados:
+            if dateutil.parser.parse(diaABuscar).date() == dateutil.parser.parse(fila[3]).date():
+                nombre = fila[1]
+                link = fila[2]
+                fecha = fila[3]
+                resultados += nombre + "\n" + link + "\n" + fecha + "\n\n"
+        conexionBuscarDia.close()
+        msg = messagebox.showinfo("Elementos BD por fecha dada", resultados)
+    
+    entradaDia.bind("<Return>", metodoDeBuscar)
+    ventanaBuscarDia.mainloop()
+
+
 
 #Creacion de la interfaz
 ventana = tk.Tk()
@@ -82,8 +127,13 @@ bListar.place(x=largoAlmacenar+1, y=0)
 bListar.update()
 largoListar = bListar.winfo_width()
 
-bBuscar = tk.Button(ventana, text="Buscar")
-bBuscar.place(x=largoAlmacenar+largoListar+1, y=0)
+bBuscarMes = tk.Button(ventana, text="Buscar mes", command = buscarMes)
+bBuscarMes.place(x=largoAlmacenar+largoListar+1, y=0)
+bBuscarMes.update()
+largoBuscarMes = bBuscarMes.winfo_width()
+
+bBuscarDia = tk.Button(ventana, text="Buscar día", command = buscarDia)
+bBuscarDia.place(x=largoAlmacenar+largoListar+largoBuscarMes+1, y=0)
 
 
 #Abrir la interfaz y cerrar la BD
