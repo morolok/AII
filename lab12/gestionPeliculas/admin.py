9de9be49850a51c969cf5bee825ebc0d5962cdc2
Carlos Mata <carlosmatauniversidad@gmail.com>
2020-05-04 12:09:53 +0200

    Ejercicio 2 Django

--- lab12/gestionPeliculas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from gestionPeliculas.models import Pais, Director, Genero, Pelicula
-
-# Register your models here.
-
-admin.site.register(Pais)
-admin.site.register(Director)
-admin.site.register(Genero)
-admin.site.register(Pelicula)
--- lab12/gestionPeliculas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionpeliculasConfig(AppConfig):
-    name = 'gestionPeliculas'
